348485d8d710168fdd8e6b5a7f70ac93c56454e0
Dhruv Kulkarni <coolnerdyboy22@gmail.com>
2023-08-13 18:08:11 +0530

    Added topcard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import TopCard from './components/TopCard';
-
+import Section from './components/Section';
 function App() {
   return (
+<div>
     <TopCard />
+    <Section />
+    </div>
   );
+
 }
 
 export default App;
